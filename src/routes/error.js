7bd6178e76969352c9c07b972f0c59800d6881aa
Jennifer Moore <jenniferplusplus@gmail.com>
2019-07-18 22:52:43 -0500

    removed index page

--- src/routes/error.js
+++ src/routes/error.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 // Global error handler
-router.use(function (req, res, next, err) {
-    res.end(err);
-});
+router.use(_404);
+router.use('/', _404);
+router.use(_500)
 
 module.exports = router;
+
+function _404 (req, res, next) {
+    return res.status(404).end()
+}
+
+function _500 (err, req, res, next) {
+    return res.json(err);
+}
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-var index = require('./index');
 var upload = require('./upload');
 var error = require('./error');
 var data = require('./data');
 
-router.use(index);
 router.use(upload);
-router.use(error);
 router.use(data);
+
+// Must be last
+router.use(error);
+
 
 module.exports = router;
